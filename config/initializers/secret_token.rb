f7806c8cc279ba16172b3cae51137d6687a5fafe
zonoise <zonoise@gmail.com>
2012-02-25 19:45:10 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsanamaBattle::Application.config.secret_token = '6aa9ba93430cd448c095932c18d07f11efb2a10431bdcc026a5f86ab437e16f4ba0c6456eb3dd89b96f3924866d0d48b8c76a41b560a193cfe12c8b144041389'
